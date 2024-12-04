d5cc8eddfdb0b6a24b1dad1efc98ba91ad6fa947
masud2005 <masud20012005@gmail.com>
2024-12-04 17:05:13 +0600

    Setup server with Express and MongoDB connection

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors())
+
+// sportsEquipments
+// DauPmoQtWXAhU0r6
+
+const uri = "mongodb+srv://sportsEquipments:DauPmoQtWXAhU0r6@cluster0.cckud.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async(req, res) => {
+    res.send("Sports Equipment Server is Running");
+})
+
+app.listen(port, () => {
+    console.log(`Sports Equipments Server is Running on PORT: ${port}`);
+})
